dff87d6b9b83dcef36e4c843461aa210013e8620
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2020-01-26 17:32:57 +0100

    done

--- src/contectful.js
+++ src/contectful.js
@@ -1,7 +1,7 @@
 import {createClient} from "contentful";
 
 export default createClient({
-    space: 'k36vndb1kj2w',
-    accessToken: 'mlw6jrijNnw5Un2w7vw0at5MJfYQmItHRqR8wr3zwUo'
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 
 })
